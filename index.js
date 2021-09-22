9cd495788062e70a28c41eebd557b70ce1ed4a89
Xila Luna <xilaluna2@gmail.com>
2021-09-21 17:33:14 -0700

    [add] files

---
+++ index.js
@@ -0,0 +1,10 @@
+import serverlessHttp from "serverless-http"
+import express from "express"
+
+const app = express()
+
+app.get("/", function (req, res) {
+  res.send("Hello World")
+})
+
+module.exports.handler = serverless(app)
