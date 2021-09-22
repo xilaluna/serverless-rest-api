eac38fd71ffc7e1a4673e623e5a88dbd6d2a3760
Xila Luna <xilaluna2@gmail.com>
2021-09-21 18:27:53 -0700

    +dynamodb serveless file & imports

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-import serverlessHttp from "serverless-http"
-import express from "express"
+const serverless = require("serverless-http")
+const bodyParser = require("body-parser")
+const express = require("express")
 
 const app = express()
+const AWS = require("aws-sdk")
+
+const USERS_TABLE = process.env.USERS_TABLE
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
+app.use(bodyParser.json({ strict: false }))
 
 app.get("/", function (req, res) {
-  res.send("Hello World")
+  res.send("Hello World!")
 })
 
 module.exports.handler = serverless(app)
